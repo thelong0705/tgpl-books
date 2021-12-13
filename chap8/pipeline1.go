65869ff6584be7b6fbbf6db1d13fee96b1b39018
thelong0705 <doan.the.long@moneyforward.co.jp>
2021-12-13 10:34:20 +0900

    unidirectional channnel

---
+++ chap8/pipeline1.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main() {
+	naturals := make(chan int)
+	squares := make(chan int)
+
+	// Counter
+	go func() {
+		for x := 0; x < 10 ; x++ {
+			naturals <- x
+		}
+		close(naturals)
+	}()
+
+	// Squarer
+	go func() {
+		for {
+			x, ok := <-naturals
+			if !ok {
+				break
+			}
+			squares <- x * x
+		}
+		close(squares)
+	}()
+
+	for {
+		x, ok := <-squares
+		if !ok {
+			break
+		}
+		fmt.Println(x)
+	}
+}
