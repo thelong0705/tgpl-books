3740f89486e58a8d8e1c934901586803b63d0a14
thelong0705 <doan.the.long@moneyforward.co.jp>
2021-12-21 10:02:44 +0900

    cancellation

---
+++ chap8/du4.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"sync"
+	"time"
+)
+
+var done = make(chan struct{})
+
+func cancelled() bool {
+	select {
+	case <-done:
+		return true
+	default:
+		return false
+	}
+}
+
+func main() {
+	roots := os.Args[1:]
+	if len(roots) == 0 {
+		roots = []string{"."}
+	}
+
+	go func() {
+		os.Stdin.Read(make([]byte, 1))
+		close(done)
+	}()
+
+	fileSizes := make(chan int64)
+	var n sync.WaitGroup
+	for _, root := range roots {
+		n.Add(1)
+		go walkDir(root, &n, fileSizes)
+	}
+
+	go func() {
+		n.Wait()
+		close(fileSizes)
+	}()
+
+	tick := time.Tick(500 * time.Millisecond)
+	var nfiles, nbytes int64
+
+loop:
+	for {
+		select {
+		case <-done:
+			for range fileSizes {
+
+			}
+			return
+		case size, ok := <-fileSizes:
+			if !ok {
+				break loop
+			}
+			nfiles++
+			nbytes += size
+		case <-tick:
+			printDiskUsage(nfiles, nbytes)
+		}
+	}
+	printDiskUsage(nfiles, nbytes)
+}
+
+func printDiskUsage(nfiles, nbytes int64) {
+	fmt.Printf("%d files  %.1f GB\n", nfiles, float64(nbytes)/1e9)
+}
+
+func walkDir(dir string, n *sync.WaitGroup, filesizes chan<- int64) {
+	defer n.Done()
+	if cancelled() {
+		return
+	}
+
+	for _, entry := range dirents(dir) {
+		if entry.IsDir() {
+			n.Add(1)
+			subdir := filepath.Join(dir, entry.Name())
+			go walkDir(subdir, n, filesizes)
+		} else {
+			filesizes <- entry.Size()
+		}
+	}
+}
+
+var sema = make(chan struct{}, 20)
+
+func dirents(dir string) []os.FileInfo {
+	select {
+	case sema <- struct{}{}:
+	case <-done:
+		return nil
+	}
+	defer func() { <-sema }()
+	entries, err := ioutil.ReadDir(dir)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "du1: %v \n", err)
+		return nil
+	}
+	return entries
+}
