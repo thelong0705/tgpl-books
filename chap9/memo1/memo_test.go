79dd979b75e309ba6c742e97b330caff7a12af36
thelong0705 <doan.the.long@moneyforward.co.jp>
2021-12-29 19:05:30 +0900

    memo1

---
+++ chap9/memo1/memo_test.go
@@ -0,0 +1,23 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+package memo_test
+
+import (
+	memo "chap9/memo1"
+	"chap9/memotest"
+	"testing"
+)
+
+var httpGetBody = memotest.HTTPGetBody
+
+func Test(t *testing.T) {
+	m := memo.New(httpGetBody)
+	memotest.Sequential(t, m)
+}
+
+// NOTE: not concurrency-safe!  Test fails.
+func TestConcurrent(t *testing.T) {
+	m := memo.New(httpGetBody)
+	memotest.Concurrent(t, m)
+}
